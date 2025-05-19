51638dcbe664127e83e465f1161b1e8587a20371
surendrasinghshekhawat <surendrasingh55556666@gmail.com>
2025-05-19 14:19:28 +0530

    second

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 let button=document.querySelectorAll(".box");
+const colors=["red","blue","yellow","pink","black","green","purpal","orange"]
 button.forEach(box =>{
     box.addEventListener("click",()=>{
-    // box.style.backgroundColor="red";
-    box.style.border="solid"
-    box.style.backgroundImage = "url('https://m.media-amazon.com/images/I/71NQ8HuXTmL._AC_UF1000,1000_QL80_.jpg')";
-    box.style.backgroundSize = "cover";     
-    box.style.backgroundPosition = "center";
-    box.style.backgroundRepet = "no-repet";
+         const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    box.style.backgroundColor = randomColor;
+
+    // // box.style.backgroundColor="red";
+    // box.style.border="solid"
+    // box.style.backgroundImage = "url('https://m.media-amazon.com/images/I/71NQ8HuXTmL._AC_UF1000,1000_QL80_.jpg')";
+    // box.style.backgroundSize = "cover";     
+    // box.style.backgroundPosition = "center";
+    // box.style.backgroundRepet = "no-repet";
 
     })
 })
